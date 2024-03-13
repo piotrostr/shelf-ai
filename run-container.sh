f5e4e016c2010e7d4e83acccf3979f4d4c94c4a6
Piotr <piotr.ostrowski@aol.com>
2024-03-13 19:29:23 +0100

    add script for building, pushing and running the ts container

---
+++ run-container.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker build \
+  -t piotrostr/shelf-ai-retail-yolo:0.1.0 \
+  -f Dockerfile \
+  .
+
+docker push piotrostr/shelf-ai-retail-yolo:0.1.0
+
+docker run \
+  --gpus all \
+  --rm \
+  -p 8080:8080 \
+  -p 8081:8081 \
+  -p 8082:8082 \
+  -it piotrostr/shelf-ai-retail-yolo:0.1.0
+
