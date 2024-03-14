d9b0ee65f24e0887a9b18aeb2403449b83d28ddc
Piotr <piotr.ostrowski@aol.com>
2024-03-14 19:02:55 +0100

    test for all three cases: file upload (non-vertex AI), batch predict, single image predict

--- test-ts.sh
+++ test-ts.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 
-curl -s -X POST http://localhost:8080/predictions/retail-yolo -T ./sample_image.jpg 
+# check if works for image files
+curl -s -X POST http://localhost:8080/predictions/retail-yolo -T ./sample_image.jpg
 
-# check if works for JSON with base64 encoded image
+# check if works for JSON with base64 encoded images
 cat > ./sample_request.json <<END
 {
   "instances": [
@@ -17,4 +18,35 @@ END
 curl -s -X POST \
   -H "Content-Type: application/json; charset=utf-8" \
   -d @sample_request.json \
+  http://localhost:8080/predictions/retail-yolo/
+
+# check if works for JSON with base64 encoded images batches
+cat > ./sample_request_batch.json <<END
+{
+  "instances": [
+    {
+      "data": "$(base64 -w 0 -i ./sample_image.jpg)"
+    },
+    { 
+      "data": "$(base64 -w 0 -i ./sample_image.jpg)"
+    },
+    { 
+      "data": "$(base64 -w 0 -i ./sample_image.jpg)"
+    },
+    { 
+      "data": "$(base64 -w 0 -i ./sample_image.jpg)"
+    },
+    { 
+      "data": "$(base64 -w 0 -i ./sample_image.jpg)"
+    },
+    { 
+      "data": "$(base64 -w 0 -i ./sample_image.jpg)"
+    }
+  ]
+}
+END
+
+curl -s -X POST \
+  -H "Content-Type: application/json; charset=utf-8" \
+  -d @sample_request_batch.json \
   http://localhost:8080/predictions/retail-yolo/
