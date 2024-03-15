a833f259ac412fb53c494c9f72ee44f95aa04984
Piotr Ostrowski <piotrostr@google.com>
2024-03-15 01:04:22 +0100

    add option of using a mask

--- infer.py
+++ infer.py
@@ -34,7 +34,7 @@ def track(args, endpoint):
     capture.release()
 
 
-def visualize(img, preds):
+def visualize(img, preds, use_mask=False):
     bounding_boxes = []
     for detection in preds:
         x1 = int(detection['box']['x1'])
@@ -47,18 +47,20 @@ def visualize(img, preds):
         cv2.putText(img, detection['name'], (x1, y1-10),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 
-    import numpy as np
-    mask = np.zeros(img.shape[:2], dtype="uint8")
-    for (x1, y1, x2, y2) in bounding_boxes:
-        cv2.rectangle(mask, (x1, y1), (x2, y2), 255, -1)
+    if use_mask:
+        import numpy as np
+        mask = np.zeros(img.shape[:2], dtype="uint8")
+        for (x1, y1, x2, y2) in bounding_boxes:
+            cv2.rectangle(mask, (x1, y1), (x2, y2), 255, -1)
+        mask_inv = cv2.bitwise_not(mask)
+        blurred = cv2.GaussianBlur(img, (21, 21), 0)  # Adjust blur strength
+        result = cv2.bitwise_and(blurred, blurred, mask=mask_inv)
+        result = cv2.add(result, img, mask=mask)
 
-    mask_inv = cv2.bitwise_not(mask)
-    blurred = cv2.GaussianBlur(img, (21, 21), 0)  # Adjust blur strength
-    result = cv2.bitwise_and(blurred, blurred, mask=mask_inv)
-    result = cv2.add(result, img, mask=mask)
-
-    combined_image = np.hstack([img, result])
-    cv2.imshow('detections', combined_image)
+        combined_image = np.hstack([img, result])
+        cv2.imshow('detections', combined_image)
+        return
+    cv2.imshow('detections', img)
 
 
 def compress(img: MatLike, quality: int = 40):
@@ -84,6 +86,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--visualize", action="store_true")
     parser.add_argument("--track", action="store_true")
+    parser.add_argument("--use_mask", action="store_true")
     args = parser.parse_args()
 
     ENDPOINT_ID = "2553457425835360256"
@@ -124,7 +127,8 @@ if __name__ == "__main__":
         print("Image Shape:", images_resized[i].shape)
 
         if args.visualize:
-            visualize(images_resized[i], res.predictions[0])
+            visualize(images_resized[i],
+                      res.predictions[0], use_mask=args.use_mask)
             cv2.waitKey(0)
 
             cv2.destroyAllWindows()
