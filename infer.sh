4a660dbc03ba123cd5cdaa5fbbcd66d2711ebd05
Piotr Ostrowski <piotrostr@google.com>
2024-03-15 16:11:17 +0100

    write JSON res.json infer.sh response

--- infer.sh
+++ infer.sh
@@ -13,13 +13,16 @@ cat > ./sample_request.json <<END
 }
 END
 
-curl \
+res=$(curl \
 -X POST \
 -H "Authorization: Bearer $(gcloud auth print-access-token)" \
 -H "Content-Type: application/json" \
 -H "Accept: application/json" \
 https://europe-west4-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/europe-west4/endpoints/${ENDPOINT_ID}:predict \
--d @./sample_request.json
+-d @./sample_request.json)
+
+# write the json res
+echo $res > ./res.json
 
 cat > ./sample_request_batch.json <<END
 {
